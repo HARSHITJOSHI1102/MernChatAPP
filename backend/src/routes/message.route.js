2a9f3fb976fabb2019766a4ebf5bbc0e1f5b1c18
Harshit <harshitjoshi1102@gmail.com>
2025-04-26 14:37:31 +0530

    Latest

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,17 +1,12 @@
-import express from "express"
-import { protectRoute } from "../middleware/authMiddleware.js"
-import { deleteMessage, getMessages, getUsersForSidebar, sendMessage } from "../controllers/messageController.js"
-import upload from "../middleware/multer.js"
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
-const router= express.Router()
+const router = express.Router();
 
-router.get("/users",protectRoute,getUsersForSidebar)
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
-router.get("/:id", protectRoute, getMessages)
-
-router.post("/send/:id",protectRoute,upload.single('image'),sendMessage)
-
-// delete chats
-router.delete("/delete/:id", protectRoute, deleteMessage)
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
